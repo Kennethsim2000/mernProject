69998f1b8742ac4454b5ff3b365e1ad43e0dcc8e
kennethsim2000 <e0725254@u.nus.edu>
2022-10-20 23:52:37 +0800

    Edit Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -7,15 +7,15 @@ import { useNavigate } from 'react-router-dom';
 function Login() {
 
   const navigate = useNavigate();
-  const [posts, setPosts] = useState("");
+  const [posts, setPosts] = useState(""); //post is used to store the error message
 
-  const [values, setValues] = useState({
+  const [values, setValues] = useState({ //values have 
     username:"",
     password: "",
   });
 
-  /*handleLogin handles the onclick of the login button. It does a post request on the /Login route
-   */
+  /*handleLogin handles the onclick of the login button. It does a post request on the /Login route, each
+  returns a response whether anot a user is found in the database.*/
   const handleLogin = (event) => {
     event.preventDefault();
     let databody={
@@ -46,7 +46,7 @@ function Login() {
   .catch(error=>console.log('Error posting in react'));
   };
 
- 
+  /*Used to update state of input boxes. */
   const handleChange = (event) => {
     setValues({
       ...values,
@@ -57,16 +57,11 @@ function Login() {
   return (
     <div className="body">
       <Navbar/>
+
         <form className="signupform">
-          <h1 style={{justifyContent:"center",marginTop:'0px'}}>Login to your account</h1>
-          <hr
-          style={{
-            marginTop: '0px',
-            backgroundColor:"white"
-          }}
-          />
+          <h1 className="loginHeader" >Login to your account</h1>
+          <hr className="loginLine"/>
           <input
-              style={{marginTop:"20px"}}
               className="inputLogin"
               type="text"
               name="username"
@@ -75,7 +70,6 @@ function Login() {
               value={values.username}
           />
           <input
-              style={{marginTop:"20px"}}
               className="inputLogin"
               type="password"
               name="password"
@@ -83,21 +77,16 @@ function Login() {
               onChange={handleChange}
               value={values.password}
           />
-        
-
-          <div style={{}}>
-          <div style={{marginTop:"15px", display:'inline-block'}}>
-            <a href="/forgot" style={{color:"white", textDecoration:"none"}}>Forgot Password?</a>
+          <div >
+            <div style={{marginTop:"15px", display:'inline-block'}}>
+              <a href="/forgot" style={{color:"white", textDecoration:"none"}}>Forgot Password?</a>
+            </div>
+            <div className="loginError" >
+              {posts}
+            </div>
           </div>
-          <div className="loginError" style={{display:'inline-block',marginLeft:'7rem' }}>
-            {posts}
-          </div>
 
-          </div>
-        
-
-          <button style={{marginTop:"20px", marginBottom:"10px"}} 
-          className="but" onClick={handleLogin} >
+          <button className="but" onClick={handleLogin} >
             Log In
           </button>
           
